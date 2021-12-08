74e9fab5237c965eb9ade439b21949ab1775f728
Nekoism <80365717+nekoism@users.noreply.github.com>
2021-12-08 12:34:28 -0500

    Create temperature.cpp

---
+++ temperature.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// Fahrenheit to Celsius
+int main() {
+//   Store the Fahrenheit here 
+  double tempf = 42;
+  double tempc = (tempf - 32)/1.8;
+//   Print the temperature
+  std::cout << "The temp is" << tempc << "degrees Celsius.\n"
+  
+}
